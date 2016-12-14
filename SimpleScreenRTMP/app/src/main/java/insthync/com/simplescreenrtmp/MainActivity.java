3cf556edfa09333a4d97f6aedf256f703cf7c00f
Ittipon Teerapruettikulchai <ittipon@infinitylevels.com>
2016-12-14 19:33:34 +0700

    Initial project files

---
+++ SimpleScreenRTMP/app/src/main/java/insthync/com/simplescreenrtmp/MainActivity.java
@@ -0,0 +1,210 @@
+package insthync.com.simplescreenrtmp;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import me.lake.librestreaming.client.RESAudioClient;
+import me.lake.librestreaming.model.RESConfig;
+import me.lake.librestreaming.model.RESCoreParameters;
+import me.lake.librestreaming.rtmp.RESFlvData;
+import me.lake.librestreaming.rtmp.RESFlvDataCollecter;
+import me.lake.librestreaming.rtmp.RESRtmpSender;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
+    private static final int REQUEST_SCREEN_CAPTURE = 1;
+    private static final int REQUEST_STREAM = 2;
+    private static String[] PERMISSIONS_STREAM = {
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    // RTMP Constraints
+    private static final String DEFAULT_RMTP_HOST = "192.168.1.78";
+    private static final int DEFAULT_RTMP_PORT = 1935;
+    private static final String DEFAULT_APP_NAME = "live";
+    private static final String DEFAULT_PUBLISH_NAME = "test";
+    // General
+    private MediaProjectionManager mMediaProjectionManager;
+    private ScreenRecorder mScreenRecorder;
+    private Button mButton;
+    private EditText mTextServerAddress;
+    private EditText mTextServerPort;
+    private EditText mTextAppName;
+    private EditText mTextPublishName;
+    // RTMP
+    boolean authorized = false;
+    private RESRtmpSender rtmpSender;
+    private RESFlvDataCollecter dataCollecter;
+    private RESCoreParameters coreParameters;
+    private RESAudioClient audioClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mButton = (Button) findViewById(R.id.toggle);
+        mButton.setOnClickListener(this);
+        mButton.setText("Start recorder");
+        mTextServerAddress = (EditText) findViewById(R.id.serverAddress);
+        mTextServerPort = (EditText) findViewById(R.id.serverPort);
+        mTextAppName = (EditText) findViewById(R.id.appName);
+        mTextPublishName = (EditText) findViewById(R.id.publishName);
+        mTextServerAddress.setText(DEFAULT_RMTP_HOST);
+        mTextServerPort.setText(""+DEFAULT_RTMP_PORT);
+        mTextAppName.setText(DEFAULT_APP_NAME);
+        mTextPublishName.setText(DEFAULT_PUBLISH_NAME);
+        //noinspection ResourceType
+        mMediaProjectionManager = (MediaProjectionManager) getSystemService(MEDIA_PROJECTION_SERVICE);
+
+        coreParameters = new RESCoreParameters();
+        coreParameters.printDetailMsg = false;
+        coreParameters.senderQueueLength = 150;
+
+        verifyPermissions();
+    }
+
+    public void verifyPermissions() {
+        int CAMERA_permission = ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA);
+        int RECORD_AUDIO_permission = ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.RECORD_AUDIO);
+        int WRITE_EXTERNAL_STORAGE_permission = ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (CAMERA_permission != PackageManager.PERMISSION_GRANTED ||
+                RECORD_AUDIO_permission != PackageManager.PERMISSION_GRANTED ||
+                WRITE_EXTERNAL_STORAGE_permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    MainActivity.this,
+                    PERMISSIONS_STREAM,
+                    REQUEST_STREAM
+            );
+            authorized = false;
+        } else {
+            authorized = true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_STREAM) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[2] == PackageManager.PERMISSION_GRANTED) {
+                authorized = true;
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        MediaProjection mediaProjection = mMediaProjectionManager.getMediaProjection(resultCode, data);
+        if (mediaProjection == null) {
+            Log.e("@@", "media projection is null");
+            return;
+        }
+
+        mScreenRecorder = new ScreenRecorder(coreParameters, 1, mediaProjection, dataCollecter);
+        mScreenRecorder.start();
+        rtmpSender.start(coreParameters.rtmpAddr);
+        audioClient.start(dataCollecter);
+
+        mButton.setText("Stop Recorder");
+        Toast.makeText(this, "Screen recorder is running...", Toast.LENGTH_SHORT).show();
+        moveTaskToBack(true);
+    }
+
+    @Override
+    public void onClick(View v) {
+        final String serverAddress = mTextServerAddress.getText().toString();
+        final int serverPort = Integer.parseInt(mTextServerPort.getText().toString());
+        final String appName = mTextAppName.getText().toString();
+        final String publishName = mTextPublishName.getText().toString();
+        final String rmptAddress = "rtmp://" + serverAddress + ":" + serverPort + "/" + appName + "/" + publishName;
+        if (rtmpSender != null || mScreenRecorder != null) {
+            release();
+            mButton.setText("Start recorder");
+        } else {
+            DisplayMetrics displaymetrics = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+
+            // video size
+            final int width = displaymetrics.widthPixels / 3;
+            final int height = displaymetrics.heightPixels / 3;
+            final int bitrate = 600000;
+            // Video setup
+            coreParameters.rtmpAddr = rmptAddress;
+            coreParameters.videoWidth = width;
+            coreParameters.videoHeight = height;
+            coreParameters.mediacdoecAVCBitRate = bitrate;
+            // Audio setup
+            audioClient = new RESAudioClient(coreParameters);
+            audioClient.prepare(RESConfig.obtain());
+
+            rtmpSender = new RESRtmpSender();
+            rtmpSender.prepare(coreParameters);
+
+            dataCollecter = new RESFlvDataCollecter() {
+                @Override
+                public void collect(RESFlvData flvData, int type) {
+                    rtmpSender.feed(flvData, type);
+                }
+            };
+
+            coreParameters.done = true;
+            Log.d(TAG, "===INFO===coreParametersReady:");
+            Log.d(TAG, coreParameters.toString());
+
+            Intent captureIntent = mMediaProjectionManager.createScreenCaptureIntent();
+            startActivityForResult(captureIntent, REQUEST_SCREEN_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        release();
+    }
+
+    private void release()
+    {
+        if (mScreenRecorder != null) {
+            mScreenRecorder.quit();
+            mScreenRecorder = null;
+        }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                if (rtmpSender != null) {
+                    rtmpSender.stop();
+                    rtmpSender.destroy();
+                    rtmpSender = null;
+                }
+
+                if (audioClient != null) {
+                    audioClient.stop();
+                    audioClient.destroy();
+                    audioClient = null;
+                }
+            }
+        }).start();
+    }
+}
---
+++ SimpleScreenRTMP/app/src/main/java/insthync/com/simplescreenrtmp/ScreenRecorder.java
@@ -0,0 +1,205 @@
+package insthync.com.simplescreenrtmp;
+
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
+import android.media.MediaFormat;
+import android.media.MediaMuxer;
+import android.media.projection.MediaProjection;
+import android.util.Log;
+import android.view.Surface;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import me.lake.librestreaming.core.Packager;
+import me.lake.librestreaming.model.RESCoreParameters;
+import me.lake.librestreaming.rtmp.RESFlvData;
+import me.lake.librestreaming.rtmp.RESFlvDataCollecter;
+import me.lake.librestreaming.rtmp.RESRtmpSender;
+import me.lake.librestreaming.tools.LogTools;
+
+public class ScreenRecorder extends Thread {
+    private static final String TAG = "ScreenRecorder";
+
+    private int mWidth;
+    private int mHeight;
+    private int mBitRate;
+    private int mDpi;
+    private MediaProjection mMediaProjection;
+    // parameters for the encoder
+    private static final String MIME_TYPE = MediaFormat.MIMETYPE_VIDEO_AVC; // H.264 Advanced Video Coding
+    private static final int FRAME_RATE = 30; // 30 fps
+    private static final int IFRAME_INTERVAL = 5; // 10 seconds between I-frames
+
+    private MediaCodec mEncoder;
+    private Surface mSurface;
+    private AtomicBoolean mQuit = new AtomicBoolean(false);
+    private MediaCodec.BufferInfo mBufferInfo = new MediaCodec.BufferInfo();
+    private VirtualDisplay mVirtualDisplay;
+    // Send data to server
+    private static final long WAIT_TIME = 5000;
+    private MediaCodec.BufferInfo eInfo;
+    private long startTime = 0;
+    private RESFlvDataCollecter dataCollecter;
+
+    public ScreenRecorder(RESCoreParameters coreParameters, int dpi, MediaProjection mp, RESFlvDataCollecter flvDataCollecter) {
+        super(TAG);
+        mWidth = coreParameters.videoWidth;
+        mHeight = coreParameters.videoHeight;
+        mBitRate = coreParameters.mediacdoecAVCBitRate;
+        mDpi = dpi;
+        mMediaProjection = mp;
+        eInfo = new MediaCodec.BufferInfo();
+        startTime = 0;
+        dataCollecter = flvDataCollecter;
+    }
+
+    /**
+     * stop task
+     */
+    public final void quit() {
+        mQuit.set(true);
+    }
+
+    @Override
+    public void run() {
+        try {
+            try {
+                prepareEncoder();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            mVirtualDisplay = mMediaProjection.createVirtualDisplay(TAG + "-display",
+                    mWidth, mHeight, mDpi, DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC,
+                    mSurface, null, null);
+            Log.d(TAG, "created virtual display: " + mVirtualDisplay);
+            recordVirtualDisplay();
+        } finally {
+            release();
+        }
+    }
+
+    private void recordVirtualDisplay() {
+        while (!mQuit.get()) {
+            int eobIndex = MediaCodec.INFO_TRY_AGAIN_LATER;
+            try {
+                eobIndex = mEncoder.dequeueOutputBuffer(eInfo, WAIT_TIME);
+            } catch (Exception ignored) {
+            }
+            switch (eobIndex) {
+                case MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED:
+                    LogTools.d("VideoSenderThread,MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED");
+                    break;
+                case MediaCodec.INFO_TRY_AGAIN_LATER:
+//                        LogTools.d("VideoSenderThread,MediaCodec.INFO_TRY_AGAIN_LATER");
+                    break;
+                case MediaCodec.INFO_OUTPUT_FORMAT_CHANGED:
+                    LogTools.d("VideoSenderThread,MediaCodec.INFO_OUTPUT_FORMAT_CHANGED:" +
+                            mEncoder.getOutputFormat().toString());
+                    sendAVCDecoderConfigurationRecord(0, mEncoder.getOutputFormat());
+                    break;
+                default:
+                    LogTools.d("VideoSenderThread,MediaCode,eobIndex=" + eobIndex);
+                    if (startTime == 0) {
+                        startTime = eInfo.presentationTimeUs / 1000;
+                    }
+                    /**
+                     * we send sps pps already in INFO_OUTPUT_FORMAT_CHANGED
+                     * so we ignore MediaCodec.BUFFER_FLAG_CODEC_CONFIG
+                     */
+                    if (eInfo.flags != MediaCodec.BUFFER_FLAG_CODEC_CONFIG && eInfo.size != 0) {
+                        ByteBuffer realData = mEncoder.getOutputBuffer(eobIndex);
+                        realData.position(eInfo.offset + 4);
+                        realData.limit(eInfo.offset + eInfo.size);
+                        sendRealData((eInfo.presentationTimeUs / 1000) - startTime, realData);
+                    }
+                    mEncoder.releaseOutputBuffer(eobIndex, false);
+                    break;
+            }
+        }
+    }
+
+    private void prepareEncoder() throws IOException {
+
+        MediaFormat format = MediaFormat.createVideoFormat(MIME_TYPE, mWidth, mHeight);
+        format.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
+        format.setInteger(MediaFormat.KEY_BIT_RATE, mBitRate);
+        format.setInteger(MediaFormat.KEY_FRAME_RATE, FRAME_RATE);
+        format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, IFRAME_INTERVAL);
+
+        Log.d(TAG, "created video format: " + format);
+        mEncoder = MediaCodec.createEncoderByType(MIME_TYPE);
+        mEncoder.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
+        mSurface = mEncoder.createInputSurface();
+        Log.d(TAG, "created input surface: " + mSurface);
+        mEncoder.start();
+    }
+
+    private void release() {
+        if (mEncoder != null) {
+            mEncoder.stop();
+            mEncoder.release();
+            mEncoder = null;
+        }
+        if (mVirtualDisplay != null) {
+            mVirtualDisplay.release();
+            mVirtualDisplay = null;
+        }
+        if (mMediaProjection != null) {
+            mMediaProjection.stop();
+            mMediaProjection = null;
+        }
+    }
+
+    private void sendAVCDecoderConfigurationRecord(long tms, MediaFormat format) {
+        byte[] AVCDecoderConfigurationRecord = Packager.H264Packager.generateAVCDecoderConfigurationRecord(format);
+        int packetLen = Packager.FLVPackager.FLV_VIDEO_TAG_LENGTH +
+                AVCDecoderConfigurationRecord.length;
+        byte[] finalBuff = new byte[packetLen];
+        Packager.FLVPackager.fillFlvVideoTag(finalBuff,
+                0,
+                true,
+                true,
+                AVCDecoderConfigurationRecord.length);
+        System.arraycopy(AVCDecoderConfigurationRecord, 0,
+                finalBuff, Packager.FLVPackager.FLV_VIDEO_TAG_LENGTH, AVCDecoderConfigurationRecord.length);
+        RESFlvData resFlvData = new RESFlvData();
+        resFlvData.droppable = false;
+        resFlvData.byteBuffer = finalBuff;
+        resFlvData.size = finalBuff.length;
+        resFlvData.dts = (int) tms;
+        resFlvData.flvTagType = RESFlvData.FLV_RTMP_PACKET_TYPE_VIDEO;
+        resFlvData.videoFrameType = RESFlvData.NALU_TYPE_IDR;
+        dataCollecter.collect(resFlvData, RESRtmpSender.FROM_VIDEO);
+    }
+
+    private void sendRealData(long tms, ByteBuffer realData) {
+        int realDataLength = realData.remaining();
+        int packetLen = Packager.FLVPackager.FLV_VIDEO_TAG_LENGTH +
+                Packager.FLVPackager.NALU_HEADER_LENGTH +
+                realDataLength;
+        byte[] finalBuff = new byte[packetLen];
+        realData.get(finalBuff, Packager.FLVPackager.FLV_VIDEO_TAG_LENGTH +
+                        Packager.FLVPackager.NALU_HEADER_LENGTH,
+                realDataLength);
+        int frameType = finalBuff[Packager.FLVPackager.FLV_VIDEO_TAG_LENGTH +
+                Packager.FLVPackager.NALU_HEADER_LENGTH] & 0x1F;
+        Packager.FLVPackager.fillFlvVideoTag(finalBuff,
+                0,
+                false,
+                frameType == 5,
+                realDataLength);
+        RESFlvData resFlvData = new RESFlvData();
+        resFlvData.droppable = true;
+        resFlvData.byteBuffer = finalBuff;
+        resFlvData.size = finalBuff.length;
+        resFlvData.dts = (int) tms;
+        resFlvData.flvTagType = RESFlvData.FLV_RTMP_PACKET_TYPE_VIDEO;
+        resFlvData.videoFrameType = frameType;
+        dataCollecter.collect(resFlvData, RESRtmpSender.FROM_VIDEO);
+    }
+}
